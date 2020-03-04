7290b9f9e1f7689444f894e04f0a4356a87ce7ac
Prerna <prerna1998.ps@gmail.com>
2020-03-04 23:50:47 +0530

    Test Solution for doubly linked List

--- insertInsortedDoublyLL.cpp
+++ insertInsortedDoublyLL.cpp
@@ -50,3 +50,43 @@ DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* head, int data) {
     }
     return head;
 }
+
+/*
+Test Solution
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* head, int data) {
+
+    DoublyLinkedListNode* node=new DoublyLinkedListNode(data);
+ 
+    if(!head){          //empty
+        head=node;
+        node->prev=NULL;
+        node->next=NULL;
+    }
+    else if(data<head->data){ //insertion at begn.
+        node->next=head;
+        head->prev=node;
+        head=node;
+    }
+    else{
+        DoublyLinkedListNode* tr=head;
+        while(tr){
+            if(tr->data>data){
+                node->next=tr;
+                node->prev=tr->prev;
+                tr->prev->next=node;
+                tr->prev=node;
+                break;
+            }
+            if(tr->next==NULL){
+                tr->next=node;
+                node->prev=tr;
+                node->next=NULL;
+                break;
+            }
+            tr=tr->next;
+        }
+    }
+    return head;
+}
+
+*/
